bcb1a97edba2964377ad9de0738b60654b643484
Shakil <shakil2995@gmail.com>
2025-05-03 03:02:06 +0600

    up

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3761467617"
+    serviceWorkerVersion: "3060800252"
   }
 });
